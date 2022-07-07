7d672cc7da274b92e253bd4503cdcdf4dafeecdc
Rivoson <rivosonradoniaina@gmail.com>
2022-07-07 09:51:39 +0300

    Add static  express midleware

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+app.use(express.static(__dirname + "public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
