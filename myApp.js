0391a720b69b6023a71587728390edf71115d372
Rivoson <rivosonradoniaina@gmail.com>
2022-07-07 10:29:50 +0300

    fIx

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 app.use('/public', express.static(__dirname + "/public"));
 
 app.get('/json', function (req, res) {
-    res.json({ "message": "Hello json" });
+    res.json(JSON.stringify({ message: "Hello json" }));
 });
 
 
