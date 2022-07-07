734694d283f59a2f205e8c61ec3d17b63fa4b058
Rivoson <rivosonradoniaina@gmail.com>
2022-07-07 10:16:22 +0300

    add get json path

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ if (!process.env.DISABLE_XORIGIN) {
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
-})
+});
+
+app.get('/json', function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
