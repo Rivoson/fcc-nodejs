a7d52eb0393237c49ef134df33c8b81c0853fb6e
Rivoson <rivosonradoniaina@gmail.com>
2022-07-13 16:57:20 +0300

    Add find people by name

--- myApp.js
+++ myApp.js
@@ -34,7 +34,13 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({
+    name: personName
+  }, function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  })
+  // done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
